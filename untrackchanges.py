92bfeed54236ddc92d7bd9d3e5ac205ab823ec2f
william davis <williamjsdavis@berkeley.edu>
2022-02-21 15:10:28 -0800

    Add original files

---
+++ untrackchanges.py
@@ -0,0 +1,88 @@
+import sys
+import re
+
+def add_command(text):
+    add_start_pos = [m.start() for m in re.finditer('\\\\add', text)]
+    add_end_pos = len(add_start_pos)*[None]
+    add_strings = len(add_start_pos)*[None]
+    n = len(text)
+    
+    for (i,v) in enumerate(add_start_pos):
+        add_end_pos[i], add_strings[i] = find_interior(i,v,5,text)
+    
+    new_text = '%s' % text
+    for i in reversed(range(len(add_start_pos))):
+        new_text = new_text[:add_start_pos[i]] + add_strings[i] + new_text[add_end_pos[i]:]
+    return new_text
+
+def remove_command(text):
+    remove_start_pos = [m.start() for m in re.finditer('\\\\remove', text)]
+    remove_end_pos = len(remove_start_pos)*[None]
+    remove_strings = len(remove_start_pos)*[None]
+    n = len(text)
+    
+    for (i,v) in enumerate(remove_start_pos):
+        remove_end_pos[i], remove_strings[i] = find_interior(i,v,8,text)
+    
+    new_text = '%s' % text
+    for i in reversed(range(len(remove_start_pos))):
+        new_text = new_text[:remove_start_pos[i]] + new_text[remove_end_pos[i]:]
+    return new_text
+
+def change_command(text):
+    change1_start_pos = [m.start() for m in re.finditer('\\\\change', text)]
+    change1_end_pos = len(change1_start_pos)*[None]
+    change1_strings = len(change1_start_pos)*[None]
+    change2_start_pos = len(change1_start_pos)*[None]
+    change2_end_pos = len(change1_start_pos)*[None]
+    change2_strings = len(change1_start_pos)*[None]
+    n = len(text)
+    
+    for (i,v) in enumerate(change1_start_pos):
+        # First text
+        change1_end_pos[i], change1_strings[i] = find_interior(i,v,8,text)
+        # Second text
+        change2_start_pos[i] = change1_end_pos[i]
+        change2_end_pos[i], change2_strings[i] = find_interior(i,change2_start_pos[i],1,text)
+    
+    new_text = '%s' % text
+    for i in reversed(range(len(change1_start_pos))):
+        new_text = new_text[:change1_start_pos[i]] + change2_strings[i] + new_text[change2_end_pos[i]:]
+    return new_text
+
+def find_interior(i,v,start,text):
+    bracket_start_pos = v + start
+    bracket_level = 1
+    ind = bracket_start_pos
+
+    interior_string = ''
+    while bracket_level > 0:
+        if text[ind] == '{':
+            bracket_level += 1
+        elif text[ind] == '}':
+            bracket_level -= 1
+        interior_string += text[ind]
+        ind += 1
+    
+    return ind, interior_string[:-1]
+def update_text(text):
+    text = add_command(text)
+    text = remove_command(text)
+    text = change_command(text)
+    return text
+
+if __name__ == "__main__":
+    filename = str(sys.argv[1])
+
+    with open(filename) as f:
+        raw_text = f.read()
+    
+    new_text = update_text(raw_text)
+    new_filename = filename + '.untrack'
+    
+    with open(new_filename, 'w') as f:
+        f.write(new_text)
+    print('Success!')
+    
+    
+    
